a680a8b14b001bf45f194a1c1a191172be2dabb5
NocniDuha <116986484+NocniDuha@users.noreply.github.com>
2022-11-25 01:29:47 +0100

    Add functions for day 2

--- main.js
+++ main.js
@@ -19,3 +19,23 @@ clickD1.addEventListener("click", day1, false);
 card1.addEventListener("mouseleave", outD1, false);
 
 //karta den 2//
+var clickD2 = document.getElementById("d2");
+var moreD2 = document.getElementById("moreD2");
+var card2 = document.getElementById("card2");
+
+function day2 () {
+if (moreD2.style.display === "none") {
+    moreD2.style.display = "inline";
+    clickD1.innerHTML = "Schovat";
+  } else {
+    moreD2.style.display = "none";
+    clickD2.innerHTML = "Ukázat";
+  }
+}
+
+function outD2 () {moreD2.style.display = "none"; clickD2.innerHTML = "Ukázat";}
+
+clickD2.addEventListener("click", day2, false);
+card2.addEventListener("mouseleave", outD2, false);
+
+// Karta den 3//
