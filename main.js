cea5994e8a57aea725cfa6ae928ad7cbfd218efd
NocniDuha <116986484+NocniDuha@users.noreply.github.com>
2022-11-26 20:14:33 +0100

    Update main.js

--- main.js
+++ main.js
@@ -39,3 +39,361 @@ clickD2.addEventListener("click", day2, false);
 card2.addEventListener("mouseleave", outD2, false);
 
 // Karta den 3//
+var clickD1 = document.getElementById("d1");
+var moreD1 = document.getElementById("moreD1");
+var card1 = document.getElementById("card1");
+
+function day1 () {
+if (moreD1.style.display === "none") {
+    moreD1.style.display = "inline";
+    clickD1.innerHTML = "Schovat";
+  } else {
+    moreD1.style.display = "none";
+    clickD1.innerHTML = "Ukázat";
+  }
+}
+
+function outD1 () {moreD1.style.display = "none"; clickD1.innerHTML = "Ukázat";}
+
+clickD1.addEventListener("click", day1, false);
+card1.addEventListener("mouseleave", outD1, false);
+
+// Karta den 4//
+var clickD1 = document.getElementById("d1");
+var moreD1 = document.getElementById("moreD1");
+var card1 = document.getElementById("card1");
+
+function day1 () {
+if (moreD1.style.display === "none") {
+    moreD1.style.display = "inline";
+    clickD1.innerHTML = "Schovat";
+  } else {
+    moreD1.style.display = "none";
+    clickD1.innerHTML = "Ukázat";
+  }
+}
+
+function outD1 () {moreD1.style.display = "none"; clickD1.innerHTML = "Ukázat";}
+
+clickD1.addEventListener("click", day1, false);
+card1.addEventListener("mouseleave", outD1, false);
+
+// Karta den 5//
+var clickD1 = document.getElementById("d1");
+var moreD1 = document.getElementById("moreD1");
+var card1 = document.getElementById("card1");
+
+function day1 () {
+if (moreD1.style.display === "none") {
+    moreD1.style.display = "inline";
+    clickD1.innerHTML = "Schovat";
+  } else {
+    moreD1.style.display = "none";
+    clickD1.innerHTML = "Ukázat";
+  }
+}
+
+function outD1 () {moreD1.style.display = "none"; clickD1.innerHTML = "Ukázat";}
+
+clickD1.addEventListener("click", day1, false);
+card1.addEventListener("mouseleave", outD1, false);
+
+// Karta den 6//
+var clickD1 = document.getElementById("d1");
+var moreD1 = document.getElementById("moreD1");
+var card1 = document.getElementById("card1");
+
+function day1 () {
+if (moreD1.style.display === "none") {
+    moreD1.style.display = "inline";
+    clickD1.innerHTML = "Schovat";
+  } else {
+    moreD1.style.display = "none";
+    clickD1.innerHTML = "Ukázat";
+  }
+}
+
+function outD1 () {moreD1.style.display = "none"; clickD1.innerHTML = "Ukázat";}
+
+clickD1.addEventListener("click", day1, false);
+card1.addEventListener("mouseleave", outD1, false);
+
+// Karta den 7//
+var clickD1 = document.getElementById("d1");
+var moreD1 = document.getElementById("moreD1");
+var card1 = document.getElementById("card1");
+
+function day1 () {
+if (moreD1.style.display === "none") {
+    moreD1.style.display = "inline";
+    clickD1.innerHTML = "Schovat";
+  } else {
+    moreD1.style.display = "none";
+    clickD1.innerHTML = "Ukázat";
+  }
+}
+
+function outD1 () {moreD1.style.display = "none"; clickD1.innerHTML = "Ukázat";}
+
+clickD1.addEventListener("click", day1, false);
+card1.addEventListener("mouseleave", outD1, false);
+
+// Karta den 8//
+var clickD1 = document.getElementById("d1");
+var moreD1 = document.getElementById("moreD1");
+var card1 = document.getElementById("card1");
+
+function day1 () {
+if (moreD1.style.display === "none") {
+    moreD1.style.display = "inline";
+    clickD1.innerHTML = "Schovat";
+  } else {
+    moreD1.style.display = "none";
+    clickD1.innerHTML = "Ukázat";
+  }
+}
+
+function outD1 () {moreD1.style.display = "none"; clickD1.innerHTML = "Ukázat";}
+
+clickD1.addEventListener("click", day1, false);
+card1.addEventListener("mouseleave", outD1, false);
+
+// Karta den 9//
+var clickD1 = document.getElementById("d1");
+var moreD1 = document.getElementById("moreD1");
+var card1 = document.getElementById("card1");
+
+function day1 () {
+if (moreD1.style.display === "none") {
+    moreD1.style.display = "inline";
+    clickD1.innerHTML = "Schovat";
+  } else {
+    moreD1.style.display = "none";
+    clickD1.innerHTML = "Ukázat";
+  }
+}
+
+function outD1 () {moreD1.style.display = "none"; clickD1.innerHTML = "Ukázat";}
+
+clickD1.addEventListener("click", day1, false);
+card1.addEventListener("mouseleave", outD1, false);
+
+// Karta den 10//
+var clickD1 = document.getElementById("d1");
+var moreD1 = document.getElementById("moreD1");
+var card1 = document.getElementById("card1");
+
+function day1 () {
+if (moreD1.style.display === "none") {
+    moreD1.style.display = "inline";
+    clickD1.innerHTML = "Schovat";
+  } else {
+    moreD1.style.display = "none";
+    clickD1.innerHTML = "Ukázat";
+  }
+}
+
+function outD1 () {moreD1.style.display = "none"; clickD1.innerHTML = "Ukázat";}
+
+clickD1.addEventListener("click", day1, false);
+card1.addEventListener("mouseleave", outD1, false);
+
+// Karta den 11//
+var clickD1 = document.getElementById("d1");
+var moreD1 = document.getElementById("moreD1");
+var card1 = document.getElementById("card1");
+
+function day1 () {
+if (moreD1.style.display === "none") {
+    moreD1.style.display = "inline";
+    clickD1.innerHTML = "Schovat";
+  } else {
+    moreD1.style.display = "none";
+    clickD1.innerHTML = "Ukázat";
+  }
+}
+
+function outD1 () {moreD1.style.display = "none"; clickD1.innerHTML = "Ukázat";}
+
+clickD1.addEventListener("click", day1, false);
+card1.addEventListener("mouseleave", outD1, false);
+
+// Karta den 12//
+var clickD1 = document.getElementById("d1");
+var moreD1 = document.getElementById("moreD1");
+var card1 = document.getElementById("card1");
+
+function day1 () {
+if (moreD1.style.display === "none") {
+    moreD1.style.display = "inline";
+    clickD1.innerHTML = "Schovat";
+  } else {
+    moreD1.style.display = "none";
+    clickD1.innerHTML = "Ukázat";
+  }
+}
+
+function outD1 () {moreD1.style.display = "none"; clickD1.innerHTML = "Ukázat";}
+
+clickD1.addEventListener("click", day1, false);
+card1.addEventListener("mouseleave", outD1, false);
+
+// Karta den 13//
+var clickD1 = document.getElementById("d1");
+var moreD1 = document.getElementById("moreD1");
+var card1 = document.getElementById("card1");
+
+function day1 () {
+if (moreD1.style.display === "none") {
+    moreD1.style.display = "inline";
+    clickD1.innerHTML = "Schovat";
+  } else {
+    moreD1.style.display = "none";
+    clickD1.innerHTML = "Ukázat";
+  }
+}
+
+function outD1 () {moreD1.style.display = "none"; clickD1.innerHTML = "Ukázat";}
+
+clickD1.addEventListener("click", day1, false);
+card1.addEventListener("mouseleave", outD1, false);
+
+// Karta den 14//
+var clickD1 = document.getElementById("d1");
+var moreD1 = document.getElementById("moreD1");
+var card1 = document.getElementById("card1");
+
+function day1 () {
+if (moreD1.style.display === "none") {
+    moreD1.style.display = "inline";
+    clickD1.innerHTML = "Schovat";
+  } else {
+    moreD1.style.display = "none";
+    clickD1.innerHTML = "Ukázat";
+  }
+}
+
+function outD1 () {moreD1.style.display = "none"; clickD1.innerHTML = "Ukázat";}
+
+clickD1.addEventListener("click", day1, false);
+card1.addEventListener("mouseleave", outD1, false);
+
+// Karta den 15//
+var clickD1 = document.getElementById("d1");
+var moreD1 = document.getElementById("moreD1");
+var card1 = document.getElementById("card1");
+
+function day1 () {
+if (moreD1.style.display === "none") {
+    moreD1.style.display = "inline";
+    clickD1.innerHTML = "Schovat";
+  } else {
+    moreD1.style.display = "none";
+    clickD1.innerHTML = "Ukázat";
+  }
+}
+
+function outD1 () {moreD1.style.display = "none"; clickD1.innerHTML = "Ukázat";}
+
+clickD1.addEventListener("click", day1, false);
+card1.addEventListener("mouseleave", outD1, false);
+
+// Karta den 16//
+var clickD1 = document.getElementById("d1");
+var moreD1 = document.getElementById("moreD1");
+var card1 = document.getElementById("card1");
+
+function day1 () {
+if (moreD1.style.display === "none") {
+    moreD1.style.display = "inline";
+    clickD1.innerHTML = "Schovat";
+  } else {
+    moreD1.style.display = "none";
+    clickD1.innerHTML = "Ukázat";
+  }
+}
+
+function outD1 () {moreD1.style.display = "none"; clickD1.innerHTML = "Ukázat";}
+
+clickD1.addEventListener("click", day1, false);
+card1.addEventListener("mouseleave", outD1, false);
+
+// Karta den 17//
+var clickD1 = document.getElementById("d1");
+var moreD1 = document.getElementById("moreD1");
+var card1 = document.getElementById("card1");
+
+function day1 () {
+if (moreD1.style.display === "none") {
+    moreD1.style.display = "inline";
+    clickD1.innerHTML = "Schovat";
+  } else {
+    moreD1.style.display = "none";
+    clickD1.innerHTML = "Ukázat";
+  }
+}
+
+function outD1 () {moreD1.style.display = "none"; clickD1.innerHTML = "Ukázat";}
+
+clickD1.addEventListener("click", day1, false);
+card1.addEventListener("mouseleave", outD1, false);
+
+// Karta den 18//
+var clickD1 = document.getElementById("d1");
+var moreD1 = document.getElementById("moreD1");
+var card1 = document.getElementById("card1");
+
+function day1 () {
+if (moreD1.style.display === "none") {
+    moreD1.style.display = "inline";
+    clickD1.innerHTML = "Schovat";
+  } else {
+    moreD1.style.display = "none";
+    clickD1.innerHTML = "Ukázat";
+  }
+}
+
+function outD1 () {moreD1.style.display = "none"; clickD1.innerHTML = "Ukázat";}
+
+clickD1.addEventListener("click", day1, false);
+card1.addEventListener("mouseleave", outD1, false);
+
+// Karta den 19//
+var clickD1 = document.getElementById("d1");
+var moreD1 = document.getElementById("moreD1");
+var card1 = document.getElementById("card1");
+
+function day1 () {
+if (moreD1.style.display === "none") {
+    moreD1.style.display = "inline";
+    clickD1.innerHTML = "Schovat";
+  } else {
+    moreD1.style.display = "none";
+    clickD1.innerHTML = "Ukázat";
+  }
+}
+
+function outD1 () {moreD1.style.display = "none"; clickD1.innerHTML = "Ukázat";}
+
+clickD1.addEventListener("click", day1, false);
+card1.addEventListener("mouseleave", outD1, false);
+
+// Karta den 20//
+var clickD1 = document.getElementById("d1");
+var moreD1 = document.getElementById("moreD1");
+var card1 = document.getElementById("card1");
+
+function day1 () {
+if (moreD1.style.display === "none") {
+    moreD1.style.display = "inline";
+    clickD1.innerHTML = "Schovat";
+  } else {
+    moreD1.style.display = "none";
+    clickD1.innerHTML = "Ukázat";
+  }
+}
+
+function outD1 () {moreD1.style.display = "none"; clickD1.innerHTML = "Ukázat";}
+
+clickD1.addEventListener("click", day1, false);
+card1.addEventListener("mouseleave", outD1, false);
